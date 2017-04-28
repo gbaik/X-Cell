02f721d008bddd789d0f8b59962a2f4fb4894f1d
gbaik <gideonbaik@gmail.com>
2017-04-27 19:30:02 -0700

    Add dom-util function

---
+++ client/js/test/dom-util.test.js
@@ -0,0 +1,47 @@
+const domUtil = require('../dom-util');
+const createTR = domUtil.createTR;
+const createTH = domUtil.createTH;
+const createTD = domUtil.createTD;
+const removeChildren = domUtil.removeChildren;
+
+describe('dom-util', () => {
+
+	describe('DOM creation functions', () => {
+		describe('createTR', () => {
+			it('produces valid TR element', () => {
+				const el = createTR();
+				expect(el.tagName).toBe('TR');
+			});
+		});
+			describe('createTH', () => {
+			it('produces valid TH element', () => {
+				const el = createTH();
+				expect(el.tagName).toBe('TH');
+			});
+		});
+
+			describe('createTD', () => {
+			it('produces valid TD element', () => {
+				const el = createTD();
+				expect(el.tagName).toBe('TD');
+			});
+		});
+
+	})
+	it('removes one child', () => {
+		//set up initial state
+		const parent = document.createElement('DIV');
+		const child = document.createElement('STRONG');
+		parent.appendChild(child);
+
+		//inspect initial state
+		expect(parent.childNodes.length).toBe(1);
+		expect(parent.childNodes[0]).toBe(child);
+
+		//execute code under test
+		removeChildren(parent);
+
+		//inspect resulting state
+		expect(parent.childNodes.length).toBe(0);
+	});
+});
